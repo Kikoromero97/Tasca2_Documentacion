fb8b09c527a0e256f0d277ecd0a5bcefb1092b6f
Kikoromero97 <kikoromero1997@gmail.com>
2023-06-01 21:35:28 +0200

    Clase SumaDescendent con comentarios SUBIDA

--- Algoritmes/src/es/iesmz/ed/algoritmes/SumaDescendent.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/SumaDescendent.java
@@ -1,11 +1,27 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Esta clase suma todos los dígitos de un número de forma descendente.
+ * @author: Francisco Romero Gálvez
+ * @version: 01/06/2023
+ */
+
 public class SumaDescendent {
     private final long numero;
+
+    /**
+     * Constructor para pasar el número que se desea sumar.
+     * @param numero El parámetro define el número que se va a pasar al método suma().
+     */
 
     public SumaDescendent(long numero) {
         this.numero = numero;
     }
+
+    /**
+     * Método que suma los dígitos de un número. Devuelve el resultado de la suma.
+     * @return Es la suma de los dígitos del número.
+     */
 
     public long suma() {
         String numeroString = String.valueOf(numero);
