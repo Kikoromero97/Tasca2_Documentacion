59c9773cfd8f265511ba28da63721125d6514184
Kikoromero97 <115979695+Kikoromero97@users.noreply.github.com>
2023-06-02 16:44:56 +0200

    JAVADOC Cablejat SUBIDO

---
+++ JAVADOC_Cablejat/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"es.iesmz.ed.algoritmes","c":"Cablejat","l":"Cablejat(String[])","u":"%3Cinit%3E(java.lang.String[])"},{"p":"es.iesmz.ed.algoritmes","c":"Cablejat","l":"esPotConnectar()"}];updateSearchResults();
