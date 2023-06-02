19025e4df9f3148a2590b52659cc536911c62545
Kikoromero97 <kikoromero1997@gmail.com>
2023-06-02 22:38:23 +0200

    Clase Polidivisible con comentarios SUBIDO

--- Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -1,19 +1,37 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Esta clase comprueba si un número es polidivisible.
+ *
+ * @author: Francisco Romero Gálvez
+ * @version: 02/06/2023
+ */
 public class Polidivisible {
     private long numero;
+
+    /**
+     * Constructor para pasar un número.
+     *
+     * @param numero El parámetro define un número para pasar por el constructor.
+     */
 
     public Polidivisible(long numero) {
         this.numero = numero;
     }
-        public boolean esPolidivisible() {
-            String numeroString = String.valueOf(numero);
-            for (int i = 1; i <= numeroString.length(); i++) {
-                long numSeparado = Long.parseLong(numeroString.substring(0, i));
-                if (numSeparado % i != 0) {
-                    return false;
-                }
+
+    /**
+     * Método que comprueba si un número es polidivisible, es decir, es divisible por su longitud.
+     *
+     * @return Devuelve true si el número es polidivisible. Si no lo es, devolverá false.
+     */
+    public boolean esPolidivisible() {
+        String numeroString = String.valueOf(numero);
+        for (int i = 1; i <= numeroString.length(); i++) {
+            long numSeparado = Long.parseLong(numeroString.substring(0, i));
+            if (numSeparado % i != 0) {
+                return false;
             }
-            return true;
         }
+        return true;
     }
+}
