686a93d86f995ccbab5198e3a3850fb568516ea4
Kikoromero97 <kikoromero1997@gmail.com>
2023-06-02 16:40:41 +0200

    Clase Cablejat con comentarios SUBIDO

--- Algoritmes/src/es/iesmz/ed/algoritmes/Cablejat.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Cablejat.java
@@ -1,12 +1,24 @@
 package es.iesmz.ed.algoritmes;
-
+/**
+ * Esta clase comprueba si pueden conectarse cables formando un círculo.
+ * @author: Francisco Romero Gálvez
+ * @version: 02/06/2023
+ */
 public class Cablejat {
     private String [] conectores;
 
+    /**
+     * Constructor para pasar un vector de Strings con los cables.
+     * @param conectores El parámetro define un vector con una cantidad de cables definidas.
+     */
     public Cablejat(String[] conectores) {
         this.conectores = conectores;
     }
 
+    /**
+     * Método que comprueba si el primer cable y el último pueden conectarse. Devuelve si se pueden conectar.
+     * @return Devuelve true si puede conectarse, false si no es posible.
+     */
     public boolean esPotConnectar() {
         boolean sePuede = false;
         for (int i = 0; i < conectores.length; i++) {
